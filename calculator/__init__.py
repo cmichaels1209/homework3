c18a1b9c8418c5267dfe219ec3aa5da754a4ca79
cmichaels1209 <cm765@njit.edu>
2025-02-10 17:54:48 -0500

    Added files from cloned repository

---
+++ calculator/__init__.py
@@ -0,0 +1,11 @@
+"""
+This module provides basic calculator functions.
+"""
+
+def add(a,b):
+    """Returns the sum of x and y."""
+    return a + b
+
+def subtract(a, b):
+    """Returns the difference of x and y."""
+    return a - b
