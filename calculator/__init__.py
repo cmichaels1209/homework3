9d3ad81d6e8367c20eb9628b97ab542e94ec6c4e
cmichaels1209 <cm765@njit.edu>
2025-02-11 17:52:31 -0500

    Added Multiplication and Division

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,11 +1,19 @@
 """
-This module provides basic calculator functions.
+Basic Calculator Fuctions - Addition, Subtraction, Multiplication, and Division
 """
 
 def add(a,b):
     """Returns the sum of x and y."""
     return a + b
 
-def subtract(a, b):
+def subtract(a,b):
     """Returns the difference of x and y."""
     return a - b
+
+def multiply(a,b):
+    """Returns the product of x and y."""
+    return a * b
+
+def divide(a,b):
+    """Returns the quotient of x and y."""
+    return a / b
