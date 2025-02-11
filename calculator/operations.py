fbdd939df1012b2fb1c049a3f0c8a74c9601b5ca
cmichaels1209 <cm765@njit.edu>
2025-02-11 18:11:59 -0500

    Created Operations file for Basic Functions

---
+++ calculator/operations.py
@@ -0,0 +1,19 @@
+"""
+Basic Calculator Fuctions - Addition, Subtraction, Multiplication, and Division
+"""
+
+def add(a,b):
+    """Returns the sum of x and y."""
+    return a + b
+
+def subtract(a,b):
+    """Returns the difference of x and y."""
+    return a - b
+
+def multiply(a,b):
+    """Returns the product of x and y."""
+    return a * b
+
+def divide(a,b):
+    """Returns the quotient of x and y."""
+    return a / b
