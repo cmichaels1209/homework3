dce16b9d231f39507c949262cff5714b92da06fc
cmichaels1209 <cm765@njit.edu>
2025-02-11 21:42:14 -0500

    Fixed Docstrings

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -4,22 +4,27 @@ from calculator.operations import add, subtract, multiply, divide
 class Calculator:
     @staticmethod
     def add(a, b):
-        calculation = Calculation(a, b, add)  """Pass the add function"""
+        """Pass the add function"""
+        calculation = Calculation(a, b, add)
         return calculation.get_result()
 
     @staticmethod
     def subtract(a, b):
-        calculation = Calculation(a, b, subtract)  """Pass the subtract function"""
+        """Pass the subtract function"""
+        calculation = Calculation(a, b, subtract)
         return calculation.get_result()
 
     @staticmethod
     def multiply(a, b):
-        calculation = Calculation(a, b, multiply)  """Pass the multiply function"""
+        """Pass the multiply function"""
+        calculation = Calculation(a, b, multiply)
         return calculation.get_result()
 
     @staticmethod
     def divide(a, b):
         if b == 0:
             raise ValueError("Cannot divide by zero!")
-        calculation = Calculation(a, b, divide)  """Pass the divide function"""
+        """Pass the divide function"""
+        calculation = Calculation(a, b, divide)
         return calculation.get_result()
+
