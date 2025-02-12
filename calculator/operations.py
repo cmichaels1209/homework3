e79c8480dca8a01cf4cec568d2996c5960757361
cmichaels1209 <cm765@njit.edu>
2025-02-11 21:27:59 -0500

    Changed ValueError to match

--- calculator/operations.py
+++ calculator/operations.py
@@ -16,5 +16,5 @@ def multiply(a,b):
 
 def divide(a,b):
     if b == 0: 
-        raise ValueError("Error! Cannot Divide by Zero!") 
+        raise ValueError("Cannot divide by zero!") 
     return a / b
