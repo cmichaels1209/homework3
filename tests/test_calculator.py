846cbae644ce42a4f40c27e8de2de9b73a1be97d
cmichaels1209 <cm765@njit.edu>
2025-02-11 21:22:32 -0500

    Imported multiple and divide

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,5 +1,5 @@
 #My Calculator Test
-from calculator import add, subtract
+from calculator import add, subtract, multiple, divide 
 
 def test_addition():
     #Test that addition function works    
